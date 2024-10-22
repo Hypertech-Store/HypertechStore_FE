b42e970f82769a5109c2a51da4c92992622f0e24
tuananh.204 <anhthu09112k5@gmail.com>
2024-10-22 22:38:22 +0700

    Hoàn thiện giao diện trang chủ

--- src/components/layout/client/slider-client/index.jsx
+++ src/components/layout/client/slider-client/index.jsx
@@ -1,19 +1,29 @@
-import { useRef } from "react";
-import { FaCircleChevronLeft, FaCircleChevronRight } from "react-icons/fa6";
+import { useRef, useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import Slider from "react-slick";
 import slider1 from '../../../../assets/img/slider/slider-20-1.jpg';
+import slider2 from '../../../../assets/img/slider/slider-28-2.jpg';
 
 const SliderClient = () => {
     const sliderRef = useRef();
+    const [isAnimating, setIsAnimating] = useState(false);
+    const [showNav, setShowNav] = useState(false); // State để hiển thị hoặc ẩn navigation buttons
 
     const settings = {
         dots: false,
         infinite: true,
         speed: 500,
         fade: true,
-        autoplay: false,
+        autoplay: true,  // Auto-play slide
+        autoplaySpeed: 3000,  // Tốc độ chuyển slide (3 giây)
         slidesToShow: 1,
         slidesToScroll: 1,
+        beforeChange: () => {
+            setIsAnimating(true);  // Bắt đầu animation khi slide thay đổi
+        },
+        afterChange: () => {
+            setIsAnimating(false);  // Dừng animation khi slide thay đổi xong
+        }
     };
 
     const handlePrevClick = () => {
@@ -26,67 +36,134 @@ const SliderClient = () => {
 
     return (
         <div className="slider-area">
-            <Slider {...settings} ref={sliderRef} className="slider-active">
-                <div
-                    className="single-slider-2 slider-height-20 d-flex align-items-center slider-height-res bg-img"
-                    style={{ backgroundImage: `url(${slider1})` }}
-                >
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-12">
-                                <div className="slider-content-20 slider-animated-1">
-                                    <h3 className="animated">Hurry Up</h3>
-                                    <h1 className="animated">Get 50% Offer</h1>
-                                    <p className="animated">
-                                        All Electronic Products & Instrument For This Summer.
-                                    </p>
-                                    <div className="slider-btn slider-btn-round btn-hover">
-                                        <a className="animated" href="shop.html">
-                                            SHOP NOW
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div
-                    className="single-slider-2 slider-height-20 d-flex align-items-center slider-height-res bg-img"
-                    style={{ backgroundImage: `url(${slider1})` }}
-                >
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-12">
-                                <div className="slider-content-20 slider-animated-1">
-                                    <h3 className="animated">Hurry Up</h3>
-                                    <h1 className="animated">Get 50% Offer</h1>
-                                    <p className="animated">
-                                        All Electronic Products & Instrument For This Summer.
-                                    </p>
-                                    <div className="slider-btn slider-btn-round btn-hover">
-                                        <a className="animated" href="shop.html">
-                                            SHOP NOW
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Slider>
+            <div className="slider-active owl-carousel nav-style-1 owl-dot-none"
 
-            {/* Custom navigation buttons */}
-            <div className="custom-nav">
-                <button id="prevBtn" onClick={handlePrevClick}>
-                    <FaCircleChevronLeft />
-                </button>
-                <button id="nextBtn" onClick={handleNextClick}>
-                    <FaCircleChevronRight />
-                </button>
+                style={{ position: 'relative' }}
+                onMouseEnter={() => setShowNav(true)}  // Hiển thị navigation khi hover
+                onMouseLeave={() => setShowNav(false)}  // Ẩn navigation khi không hover
+            >
+                <Slider {...settings} ref={sliderRef} >
+
+                    {/* Slide 1 */}
+                    <div className="single-slider-2 slider-height-20 d-flex align-items-center slider-height-res bg-img" style={{ position: 'relative' }}>
+                        <img
+                            src={slider1}
+                            alt="Slider Image"
+                            style={{
+                                width: '100%',
+                                height: '100vh',
+                                objectFit: 'cover',
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                zIndex: 1
+                            }}
+                        />
+
+                        <div className="container" style={{ position: 'relative', zIndex: 2 }}>
+                            <div className="row">
+                                <div className="col-12">
+                                    {/* Thêm lớp fade-out hoặc fade-in dựa trên trạng thái */}
+                                    <div className={`slider-content-20 slider-animated-1 ${isAnimating ? 'fade-out-down' : 'fade-in-up'}`}>
+                                        <h3 className="animated">Hurry Up</h3>
+                                        <h1 className="animated">Get 50% Offer</h1>
+                                        <p className="animated">
+                                            All Electronic Products & Instrument For This Summer.
+                                        </p>
+                                        <div className="slider-btn slider-btn-round btn-hover">
+                                            <a className="animated" href="shop.html">
+                                                SHOP NOW
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Slide 2 */}
+                    <div className="single-slider-2 slider-height-20 d-flex align-items-center slider-height-res bg-img" style={{ position: 'relative' }}>
+                        <img
+                            src={slider2}
+                            alt="Slider Image"
+                            style={{
+                                width: '100%',
+                                height: '100vh',
+                                objectFit: 'cover',
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                zIndex: 1,
+
+                            }}
+                        />
+
+                        <div className="container" style={{ position: 'relative', zIndex: 2 }}>
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className={`slider-content-20 slider-animated-1 ${isAnimating ? 'fade-out-down' : 'fade-in-up'}`}>
+                                        <h3 className="animated">Hurry Up</h3>
+                                        <h1 className="animated">Get 50% Offer</h1>
+                                        <p className="animated">
+                                            All Electronic Products & Instrument For This Summer.
+                                        </p>
+                                        <div className="slider-btn slider-btn-round btn-hover">
+                                            <a className="animated" href="shop.html">
+                                                SHOP NOW
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </Slider>
+
+                {/* Nút điều hướng */}
+                {showNav && ( // Chỉ hiển thị khi hover
+                    <div className="custom-nav">
+                        <button
+                            id="prevBtn"
+                            onClick={handlePrevClick}
+                            style={{
+                                position: 'absolute',
+                                top: '50%',
+                                left: '10px',
+                                transform: 'translateY(-50%)',
+                                zIndex: 3,
+                                background: 'none',
+                                border: 'none',
+                                fontSize: '30px',
+                                color: '#fff',
+                                cursor: 'pointer'
+                            }}
+                        >
+                            <FaChevronLeft />
+                        </button>
+                        <button
+                            id="nextBtn"
+                            onClick={handleNextClick}
+                            style={{
+                                position: 'absolute',
+                                top: '50%',
+                                right: '10px',
+                                transform: 'translateY(-50%)',
+                                zIndex: 3,
+                                background: 'none',
+                                border: 'none',
+                                fontSize: '30px',
+                                color: '#fff',
+                                cursor: 'pointer'
+                            }}
+                        >
+                            <FaChevronRight />
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     );
 };
 
 export default SliderClient;
-
